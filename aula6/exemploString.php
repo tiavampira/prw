cef4da3d38f48169a2f88daf9c1bc8497c737b7f
ellen <queiroz.ellen@aluno.ifsp.edu.br>
2022-03-30 10:25:49 -0300

    ll

---
+++ aula6/exemploString.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $palavra = "Ciência da Computação ";
+    echo "A variavel contem: " . strlen($palavra) . "caracter"
+    . "<br>";
+
+    $palavra = trim($palavra);
+    echo "A variavel agora contém: " . strlen($palavra) . "caracters";
+
+?>
