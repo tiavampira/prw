095912950152e56715080dd0be0edcd10eaea4c7
ellen <queiroz.ellen@aluno.ifsp.edu.br>
2022-04-06 09:13:55 -0300

    kk

---
+++ aula6/Lista2/exercicio3.php
@@ -0,0 +1,15 @@
+<?php
+
+    $num=1;
+    $primo;
+    $resultado=0;
+
+    for($primo = 2; $primo <= $num/2; $primo++);{
+        if($num%$primo == 0){
+
+            $resultado++;
+
+        }
+    }
+
+?>
