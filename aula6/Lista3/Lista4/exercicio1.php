84ce676e6bc499bbf55467a8c73e02b60c8337c0
ellen <queiroz.ellen@aluno.ifsp.edu.br>
2022-04-11 21:43:56 -0300

    Lista 3

---
+++ aula6/Lista3/Lista4/exercicio1.php
@@ -0,0 +1,51 @@
+<?php 
+
+  $resultado;
+  $opcao1 = $_GET['adicao'];
+  $opcao2 = $_GET['subtracao']; 
+  $opcao3 = $_GET['multiplicacao']; 
+  $opcao4 = $_GET['divisao'];
+
+  $num1 = $_GET['num1']; 
+  $num2 = $_GET['num2'];
+  echo "Seu número 1 é : " 
+  echo $num1; "<br>";
+
+  echo "Seu número 2 é: "
+  echo $num2; "<br>";
+  echo "<br /><br />";
+
+  switch ($resultado){
+
+    case 1($opcao1){
+      $resultado = $num1 + $num2;
+      echo $resultado++;
+      break;
+
+    }
+
+    case 2($opcao2){
+      $resultado = $num1 - $num2;
+      echo $resultado++;
+      break;
+
+    }
+
+    case 3($opcao3){
+      $resultado = $num1 * $num2;
+      echo $resultado++;
+      break;
+
+    }
+
+    case 4($opcao4){
+      $resultado =$num1 % $num2;
+      echo $resultado++;
+      break;
+
+      default 
+      echo "Erro, insira informações corretamentes";
+    }
+  }
+
+?>
