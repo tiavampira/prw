2fbfa30a994fad174fba572a62862637d2c600f0
ellen <queiroz.ellen@aluno.ifsp.edu.br>
2022-04-12 20:20:03 -0300

    sss

---
+++ aula6/Lista5/exercicio4.php
@@ -0,0 +1,28 @@
+<?php
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+$usuarios = array ('queirozellen@gmail.com' => ,'ellen12345'
+                    'nicoleyay@gmail.com' => 'nicole12345');
+
+    
+        if(array_key_exists($email, $email)){
+
+            if($usuarios[$email == $senha]){
+
+                echo 'Acesso permitido';
+            }
+            else{
+
+                echo 'Senha incorreta';
+
+            }
+        } else{
+
+            echo ' Usuário não encontrado, tente novamente';
+        }
+
+
+
+?>
