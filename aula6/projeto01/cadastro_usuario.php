96221134bc70f2368da0ab4167616018824af617
ellen <queiroz.ellen@aluno.ifsp.edu.br>
2022-04-20 11:38:49 -0300

    aaaaa

--- aula6/projeto01/cadastro_usuario.php
+++ aula6/projeto01/cadastro_usuario.php
@@ -1,5 +1,5 @@
 <?php
-
+include ('conexao.php');
  $nome_usuario = $_POST['nome_usuario'];
  $email_usuario = $_POST['email_usuario'];
  $telefone_usuario = $_POST['telefone_usuario'];
@@ -8,6 +8,15 @@
  echo "<p>E-mail: ".$email_usuario."<br>";
  echo "<p>Telefone: ".$telefone_usuario."<br>";
 
- 
+ $sql ="insert into usuario ('nome_usuario', 'email_usuario', 'telefone_usuario')
+        values ('".$nome. '" ,"'.$emai. '" ,"'.$telefone. "')";
+
+$result = mysqli_query($con, $sql);
+if($result)
+    echo "Dados inseridos dos sucessos";
+
+    else 
+    echo "Erro ao inserir no banco de dados".mysqli_error($con);
+    
 
 ?>
---
+++ aula6/projeto01/conexao.php
@@ -0,0 +1,16 @@
+<?php
+$hostname = "localhost";
+$port     = 3307;
+$username = "root";
+$password = "";
+$database = "projeto01";
+$con = mysqli_connect($hostname, $username, $password, $database, $port );
+
+if(mysqli_connect_errno()){
+
+    printf("Erro ao conectar ao banco de dados: %s\n" , mysqli_connect_errno());
+    exit;
+} 
+
+print("Banco de dados conectado com sucesso \o/")
+?>
