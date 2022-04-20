c73ff6cf990374aede1a70099633334717290b1c
ellen <queiroz.ellen@aluno.ifsp.edu.br>
2022-04-20 09:31:32 -0300

    a

---
+++ aula6/projeto01/cadastro_usuario.php
@@ -0,0 +1,13 @@
+<?php
+
+ $nome_usuario = $_POST['nome_usuario'];
+ $email_usuario = $_POST['email_usuario'];
+ $telefone_usuario = $_POST['telefone_usuario'];
+
+ echo "<p>Nome do usuario: ".$nome_usuario."<br>";
+ echo "<p>E-mail: ".$email_usuario."<br>";
+ echo "<p>Telefone: ".$telefone_usuario."<br>";
+
+ 
+
+?>
